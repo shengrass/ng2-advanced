c848ee0f19cc0a041bf0b5fecca4cb73fd90916b
shengrass <shengrass@gmail.com>
2017-04-26 15:24:21 +0800

    刪除 Page 1 和 Page 2 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 import { FlotComponent } from './charts/flot/flot.component';
@@ -14,8 +12,6 @@ import { FlotComponent } from './charts/flot/flot.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component,
-    Page2Component,
     DashboardComponent,
     CardsComponent,
     FlotComponent
